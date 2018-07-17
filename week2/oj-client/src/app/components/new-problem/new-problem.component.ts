1fe53c948fb0c3d155193ed50110346e06a8c5e6
hanshanyang2018@gmal.com <hanshanyang2018@gmail.com>
2018-07-18 06:03:35 +0800

    navbar and new-problem messages compontent

---
+++ week2/oj-client/src/app/components/new-problem/new-problem.component.ts
@@ -0,0 +1,37 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {Problem} from "../../models/problem.model";
+
+
+const DEFAULT_PROBLEMS:Problem = Object.freeze({
+  id:0,
+  name:"",
+  desc:"",
+  difficulty:""
+
+});
+@Component({
+  selector: 'app-new-problem',
+  templateUrl: './new-problem.component.html',
+  styleUrls: ['./new-problem.component.css']
+})
+export class NewProblemComponent implements OnInit {
+
+  public difficulties = ["Easy","Medium","Hard","Super"];
+
+  newProblem : Problem = Object.assign({},DEFAULT_PROBLEMS);
+
+
+  constructor(@Inject("data")private data) { }
+
+  ngOnInit() {
+  }
+
+  addProblem():void {
+    /*this.data.addProblem(this.newProblem);
+    this.newProblem = Object.assign({},DEFAULT_PROBLEMS);*/
+    this.data.addProblem(this.newProblem)
+      .catch(error => console.log(error._body));
+    this.newProblem = Object.assign({}, DEFAULT_PROBLEMS);
+
+  }
+}
