6e305db43a035664cfec76375f814719e6a30d71
hanshanyang2018@gmal.com <hanshanyang2018@gmail.com>
2018-07-18 05:50:34 +0800

    server-side done and add a disabled select option

---
+++ week2/oj-server/server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var restRouter = require("./routes/rest");
+var mongoose =  require("mongoose");
+var indexRouter = require("./routes/index");
+var path = require('path');
+
+mongoose.connect("mongodb://user:han4832069@ds135441.mlab.com:35441/coj");
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use("/",indexRouter);
+app.use("/api/v1",restRouter);
+
+
+/*app.get('/', function (req, res) {
+    res.send('Hello express world again!')
+})*/
+
+app.listen(3000, function () {
+    console.log('App listening on port 3000!')
+})
